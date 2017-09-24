664a5e2c1bb4094ca1c96a51b557e06685429116
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-24 19:49:45 +0300

    ilk yükleme
    C# Hello World

---
+++ 20170805-1Con_Giris/20170805Con_Giris/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace _20170805Con_Giris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             birdan fazla satır 
+             yorum satırı
+             oluyor.
+             */
+
+            //Bu komut bip sesi çıkarır.
+            //Console.Beep(2000,100);
+
+            //ilk görev
+            //Console.Write("Merhaba C# dünyası");
+            Console.WriteLine("merhaba");
+            Console.WriteLine("dünya");
+
+            Console.Read();
+        }
+
+        class EvYemekleri
+        {
+            void Manti() { }
+            void Kurufasulye() { }
+        }
+        class Icecekler
+        {
+            void Kola() { }
+            void Ayran() { }
+        }
+    }
+}
